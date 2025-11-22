cb584640acc433c1b19b4da74d4885f37ec6c800
Gustavo Campagnin <gcampagnin@gmail.com>
2025-11-22 15:01:59 -0300

    Allow OPTIONS requests to bypass ensureUser

--- src/modules/meals/metrics.routes.ts
+++ src/modules/meals/metrics.routes.ts
@@ -3,7 +3,13 @@ import { ensureUser } from "../../middlewares/ensure-user";
 import { MetricsController } from "./metrics.controller";
 
 export async function metricsRoutes(app: FastifyInstance) {
-  app.addHook("onRequest", ensureUser);
+  app.addHook("onRequest", async (request, reply) => {
+    if (request.method === "OPTIONS") {
+      return;
+    }
+
+    return ensureUser(request, reply);
+  });
 
   const controller = new MetricsController();
 
